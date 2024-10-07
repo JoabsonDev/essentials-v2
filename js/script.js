3805d7dd022f92dd944f626eb19e182d1e90e375
Thiago Arebalo <thiago7848ferrari@hotmail.com>
2024-10-07 18:36:24 -0300

    ask em desenvolvimento

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ document.addEventListener("scroll", () =>
     ? header.classList.add("l-header--fixed")
     : header.classList.remove("l-header--fixed")
 )
+
+document
+  .querySelectorAll(".acordeon__card__title")
+  .forEach((acordeon__card__title) => {
+    acordeon__card__title.addEventListener("click", () => {
+      const acordeon__card__content = acordeon__card__title.nextElementSibling
+      acordeon__card__content.style.display =
+        acordeon__card__content.style.display === "block" ? "none" : "block"
+    })
+  })
